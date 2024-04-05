b4cb92cbb961f146a3de089d6baa85420a411249
Rizkie Pratama <itsme@rizkiepratama.net>
2024-04-05 14:30:05 +0700

    Add Log

--- dist/videojs-dash.js
+++ dist/videojs-dash.js
@@ -69816,6 +69816,12 @@
 	  _proto.currentTime = function currentTime() {
 	    // Livestream
 	    if (this.mediaPlayer_.isDynamic()) {
+	      console.log('DIFF DASH TIME\n===============================');
+	      console.log(this.startTime());
+	      console.log(this.mediaPlayer_.time());
+	      console.log(this.mediaPlayer_.time() + this.timeDiffFromStart(this.startTime()));
+	      console.log(Date.now());
+	      console.log('===============================');
 	      return this.mediaPlayer_.time() + this.timeDiffFromStart(this.startTime());
 	    }
 
--- src/js/videojs-dash.js
+++ src/js/videojs-dash.js
@@ -297,6 +297,12 @@ class Html5DashJS {
   currentTime() {
     // Livestream
     if (this.mediaPlayer_.isDynamic()) {
+      console.log('DIFF DASH TIME\n===============================');
+      console.log(this.startTime());
+      console.log(this.mediaPlayer_.time());
+      console.log(this.mediaPlayer_.time() + this.timeDiffFromStart(this.startTime()));
+      console.log(Date.now());
+      console.log('===============================');
       return this.mediaPlayer_.time() + this.timeDiffFromStart(this.startTime());
     }
 
